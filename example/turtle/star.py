cb090b40278aa343e7af2995283738030963a8f1
Viktar Tserashchuk <viktartse@gmail.com>
2024-06-04 23:30:31 +0300

    Improve drawing at the end of line

---
+++ example/turtle/star.py
@@ -0,0 +1,12 @@
+from turtle import *
+
+speed(0)
+penup()
+goto(0, 40)
+pendown()
+
+for i in range(8):
+    forward(50)
+    right(90)
+    forward(50)
+    left(45) 
--- src/lib/turtle.js
+++ src/lib/turtle.js
@@ -1972,7 +1972,7 @@ function generateTurtleModule(_target) {
     function getRoundedOrderedCoord(x1, x2) {
         let intX1, intX2;
         [intX1, intX2] = [Math.min(x1, x2), Math.max(x1, x2)];
-        intX1 = Math.floor(intX1 + 0.3);
+        intX1 = Math.floor(intX1 + 0.8);
         intX2 = Math.floor(intX2 + 0.01);
         if (intX2 < intX1) intX2 = intX1;
         return [intX1, intX2];
