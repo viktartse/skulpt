60c057cced7476bf284d66943f1ed1776a44529d
Viktar Tserashchuk <viktartse@gmail.com>
2025-02-26 00:09:05 +0300

    Port more functions

--- src/lib/p5/__init__.js
+++ src/lib/p5/__init__.js
@@ -1,7 +1,7 @@
 /*
  *  __author__: Viktar Tserashchuk
  *
- *  Implementation of the Python robot module.
+ *  Implementation of the Python p5 module.
  */
 
 var $builtinmodule = function() {
@@ -9,6 +9,9 @@ var $builtinmodule = function() {
 
     let p5ref = null;
 
+    // Content will be later. Reference need in "run" now
+    const module = {};
+    
     // create p5 reference
     function run() {
         const sketch = p => {
@@ -27,178 +30,68 @@ var $builtinmodule = function() {
                 }
             };
         };
-
-        p5ref = new p5(sketch);
-    }
-    
-    function createCanvas(...args) {
-        Sk.builtin.pyCheckArgsLen("createCanvas", args.length, 2, 2);
-        Sk.builtin.pyCheckType("width", "integer", Sk.builtin.checkInt(args[0]));
-        Sk.builtin.pyCheckType("height", "integer", Sk.builtin.checkInt(args[1]));
-
-        remapToJsAndCall(() => p5ref.createCanvas, args);
-    }
-
-    function line(...args) {
-        Sk.builtin.pyCheckArgsLen("line", args.length, 4, 4);
-        Sk.builtin.pyCheckType("x1", "number", Sk.builtin.checkNumber(args[0]));
-        Sk.builtin.pyCheckType("y1", "number", Sk.builtin.checkNumber(args[1]));
-        Sk.builtin.pyCheckType("x2", "number", Sk.builtin.checkNumber(args[2]));
-        Sk.builtin.pyCheckType("y2", "number", Sk.builtin.checkNumber(args[3]));
-
-        remapToJsAndCall(() => p5ref.line, args);
-    }
-
-    function background(...args) {
-        Sk.builtin.pyCheckArgsLen("background", args.length, 1, 4);
-        checkColorParams(args);
-
-        remapToJsAndCall(() => p5ref.background, args);
-    }
-
-    function stroke(...args) {
-        Sk.builtin.pyCheckArgsLen("stroke", args.length, 1, 4);
-        checkColorParams(args);
-
-        remapToJsAndCall(() => p5ref.stroke, args);
-    }
-
-    function fill(...args) {
-        Sk.builtin.pyCheckArgsLen("fill", args.length, 1, 4);
-        checkColorParams(args)
-
-        remapToJsAndCall(() => p5ref.fill, args);
-    }
-
-    function checkColorParams(args) {
-        if (args.length === 1) {
-            Sk.builtin.pyCheckType("value", "number or string", checkStringOrNumber(args[0]));
-        }
-        else if (args.length === 2) {
-            Sk.builtin.pyCheckType("gray", "number", Sk.builtin.checkNumber(args[0]));
-            Sk.builtin.pyCheckType("alpha", "number", Sk.builtin.checkNumber(args[1]));
-        }
-        else if (args.length === 3) {
-            Sk.builtin.pyCheckType("v1", "number", Sk.builtin.checkNumber(args[0]));
-            Sk.builtin.pyCheckType("v2", "number", Sk.builtin.checkNumber(args[1]));
-            Sk.builtin.pyCheckType("v3", "number", Sk.builtin.checkNumber(args[2]));
-        }
-        else if (args.length === 4) {
-            Sk.builtin.pyCheckType("v1", "number", Sk.builtin.checkNumber(args[0]));
-            Sk.builtin.pyCheckType("v2", "number", Sk.builtin.checkNumber(args[1]));
-            Sk.builtin.pyCheckType("v3", "number", Sk.builtin.checkNumber(args[2]));
-            Sk.builtin.pyCheckType("alpha", "number", Sk.builtin.checkNumber(args[3]));
-        }
-    }
-    
-    function strokeWeight(...args) {
-        Sk.builtin.pyCheckArgsLen("strokeWeight", args.length, 1, 1);
-        Sk.builtin.pyCheckType("weight", "number", Sk.builtin.checkNumber(args[0]));
         
-        remapToJsAndCall(() => p5ref.strokeWeight, args);
+        p5ref = new p5(sketch);
+        checkConstants(module, p5ref);
     }
 
-    function circle(...args) {
-        Sk.builtin.pyCheckArgsLen("circle", args.length, 3, 3);
-        Sk.builtin.pyCheckType("x", "number", Sk.builtin.checkNumber(args[0]));
-        Sk.builtin.pyCheckType("y", "number", Sk.builtin.checkNumber(args[1]));
-        Sk.builtin.pyCheckType("d", "number", Sk.builtin.checkNumber(args[2]));
-
-        remapToJsAndCall(() => p5ref.circle, args);
+    function toJs(v) {
+        return Sk.ffi.remapToJs(v);
     }
+    function checkConstants(module, p5)
+    {
+        const eps = 0.00001;
+        assert(Math.abs(p5.PI - toJs(module.PI)) < eps, true);
+        assert(Math.abs(p5.QUARTER_PI - toJs(module.QUARTER_PI)) < eps, true);
+        assert(Math.abs(p5.TWO_PI - toJs(module.TWO_PI)) < eps, true);
 
-    circle.co_varnames = ["x", "y", "d"];
+        assert(p5.WEBGL, toJs(module.WEBGL));
 
-    function describe(...args) {
-        Sk.builtin.pyCheckArgsLen("describe", args.length, 1, 2);
-        Sk.builtin.pyCheckType("text", "string", Sk.builtin.checkString(args[0]));
-        if (args.length === 2) {
-            Sk.builtin.pyCheckType("display", "string", Sk.builtin.checkString(args[1]));
-        }
+        assert(p5.LABEL, toJs(module.LABEL));
+        assert(p5.FALLBACK, toJs(module.FALLBACK));
 
-        remapToJsAndCall(() => p5ref.describe, args);
-    }
+        assert(p5.OPEN, toJs(module.OPEN));
+        assert(p5.PIE, toJs(module.PIE));
+        assert(p5.CHORD, toJs(module.CHORD));
 
+        assert(p5.CENTER, toJs(module.CENTER));
+        assert(p5.RADIUS, toJs(module.RADIUS));
+        assert(p5.CORNER, toJs(module.CORNER));
+        assert(p5.CORNERS, toJs(module.CORNERS));
 
-    function rect(...args) {
-        Sk.builtin.pyCheckArgsLen("rect", args.length, 4, 8);
-        Sk.builtin.pyCheckType("x", "number", Sk.builtin.checkNumber(args[0]));
-        Sk.builtin.pyCheckType("y", "number", Sk.builtin.checkNumber(args[1]));
-        Sk.builtin.pyCheckType("w", "number", Sk.builtin.checkNumber(args[2]));
-        Sk.builtin.pyCheckType("h", "number", Sk.builtin.checkNumber(args[3]));
-        if (args.length > 4)
-            Sk.builtin.pyCheckType("tl", "number", Sk.builtin.checkNumber(args[4]));
-        if (args.length > 5)
-            Sk.builtin.pyCheckType("tr", "number", Sk.builtin.checkNumber(args[5]));
-        if (args.length > 6)
-            Sk.builtin.pyCheckType("br", "number", Sk.builtin.checkNumber(args[6]));
-        if (args.length > 7)
-            Sk.builtin.pyCheckType("bl", "number", Sk.builtin.checkNumber(args[7]));
+        assert(p5.ROUND, toJs(module.ROUND));
+        assert(p5.SQUARE, toJs(module.SQUARE));
+        assert(p5.PROJECT, toJs(module.PROJECT));
 
-        remapToJsAndCall(() => p5ref.rect, args);
-    }
+        assert(p5.MITER, toJs(module.MITER));
+        assert(p5.BEVEL, toJs(module.BEVEL));
 
-    function square(...args) {
-        Sk.builtin.pyCheckArgsLen("square", args.length, 3, 7);
-        Sk.builtin.pyCheckType("x", "number", Sk.builtin.checkNumber(args[0]));
-        Sk.builtin.pyCheckType("y", "number", Sk.builtin.checkNumber(args[1]));
-        Sk.builtin.pyCheckType("s", "number", Sk.builtin.checkNumber(args[2]));
-        if (args.length > 3)
-            Sk.builtin.pyCheckType("tl", "number", Sk.builtin.checkNumber(args[2]));
-        if (args.length > 4)
-            Sk.builtin.pyCheckType("tr", "number", Sk.builtin.checkNumber(args[4]));
-        if (args.length > 5)
-            Sk.builtin.pyCheckType("br", "number", Sk.builtin.checkNumber(args[5]));
-        if (args.length > 6)
-            Sk.builtin.pyCheckType("bl", "number", Sk.builtin.checkNumber(args[6]));
+        assert(p5.POINTS, toJs(module.POINTS));
+        assert(p5.LINES, toJs(module.LINES));
+        assert(p5.TRIANGLES, toJs(module.TRIANGLES));
+        assert(p5.TRIANGLE_FAN, toJs(module.TRIANGLE_FAN));
+        assert(p5.TRIANGLE_STRIP, toJs(module.TRIANGLE_STRIP));
+        assert(p5.QUADS, toJs(module.QUADS));
+        assert(p5.QUAD_STRIP, toJs(module.QUAD_STRIP));
+        assert(p5.TESS, toJs(module.TESS));
 
-        remapToJsAndCall(() => p5ref.square, args);
+        assert(p5.CLOSE, toJs(module.CLOSE));
     }
     
-
-    function triangle(...args) {
-        Sk.builtin.pyCheckArgsLen("triangle", args.length, 6, 6);
-        Sk.builtin.pyCheckType("x1", "number", Sk.builtin.checkNumber(args[0]));
-        Sk.builtin.pyCheckType("y1", "number", Sk.builtin.checkNumber(args[1]));
-        Sk.builtin.pyCheckType("x2", "number", Sk.builtin.checkNumber(args[2]));
-        Sk.builtin.pyCheckType("y2", "number", Sk.builtin.checkNumber(args[3]));
-        Sk.builtin.pyCheckType("x3", "number", Sk.builtin.checkNumber(args[4]));
-        Sk.builtin.pyCheckType("y3", "number", Sk.builtin.checkNumber(args[5]));
-
-        remapToJsAndCall(() => p5ref.triangle, args);
-    }
-
-    function arc(...args) {
-        Sk.builtin.pyCheckArgsLen("arc", args.length, 6, 7);
-        Sk.builtin.pyCheckType("x", "number", Sk.builtin.checkNumber(args[0]));
-        Sk.builtin.pyCheckType("y", "number", Sk.builtin.checkNumber(args[1]));
-        Sk.builtin.pyCheckType("w", "number", Sk.builtin.checkNumber(args[2]));
-        Sk.builtin.pyCheckType("h", "number", Sk.builtin.checkNumber(args[3]));
-        Sk.builtin.pyCheckType("start", "number", Sk.builtin.checkNumber(args[4]));
-        Sk.builtin.pyCheckType("stop", "number", Sk.builtin.checkNumber(args[5]));
-        if (args.length > 6)
-            Sk.builtin.pyCheckType("mode", "number", Sk.builtin.checkString(args[6]));
-
-        remapToJsAndCall(() => p5ref.arc, args);
+    function assert(expected, actual) {
+        if (actual !== expected) 
+            throw Error(`Expected '${expected} of type '${typeof expected}' but found '${actual}' of type '${typeof actual}'`);
     }
     
     function getFrameCount() {
         throwIfNoP5Reference();
         return Sk.ffi.remapToPy(p5ref.frameCount);
     }
-    
-    function noStroke() {
-        remapToJsAndCall(() => p5ref.noStroke, []);
-    }
-
-    function checkStringOrNumber(param) {
-        return Sk.builtin.checkNumber(param) || Sk.builtin.checkString(param);
-    }
 
     function throwIfNoP5Reference() {
         if (!p5ref) throw new Error("NoP5RefCreated");
     }
-    
+
     function remapToJsAndCall(actionGetter, args) {
         throwIfNoP5Reference();
 
@@ -206,34 +99,87 @@ var $builtinmodule = function() {
         actionGetter().apply(p5ref, jsArgs);
     }
     
-    const module =  {
+    function funcToPy(actionGetter) {
+        return new Sk.builtin.func((...args) => remapToJsAndCall(actionGetter, args))
+    }
+    
+    const moduleContent =  {
         __name__: new Sk.builtin.str("p5"),
 
         run: new Sk.builtin.func(run),
-        create_canvas: new Sk.builtin.func(createCanvas),
-        line: new Sk.builtin.func(line),
-        background: new Sk.builtin.func(background),
-        stroke: new Sk.builtin.func(stroke),
-        stroke_weight: new Sk.builtin.func(strokeWeight),
-        circle: new Sk.builtin.func(circle),
-        LABEL: new Sk.builtin.str("label"),
-        FALLBACK: new Sk.builtin.str("fallback"),
-        describe: new Sk.builtin.func(describe),
-        rect: new Sk.builtin.func(rect),
-        triangle: new Sk.builtin.func(triangle),
-        square: new Sk.builtin.func(square),
-        arc: new Sk.builtin.func(arc),
-        OPEN: new Sk.builtin.str("open"),
-        PIE: new Sk.builtin.str("pie"),
-        CHORD: new Sk.builtin.str("chord"),
+
+        create_canvas: funcToPy(() => p5ref.createCanvas),
+
+        arc: funcToPy(() => p5ref.arc),
+        circle: funcToPy(() => p5ref.circle),
+        ellipse: funcToPy(() => p5ref.ellipse),
+        line: funcToPy(() => p5ref.line),
+        point: funcToPy(() => p5ref.point),
+        quad: funcToPy(() => p5ref.quad),
+        rect: funcToPy(() => p5ref.rect),
+        square: funcToPy(() => p5ref.square),
+        triangle: funcToPy(() => p5ref.triangle),
+
+        ellipse_mode: funcToPy(() => p5ref.ellipseMode),
+        no_smooth: funcToPy(() => p5ref.noSmooth),
+        rect_mode: funcToPy(() => p5ref.rectMode),
+        smooth: funcToPy(() => p5ref.smooth),
+        stroke_cap: funcToPy(() => p5ref.strokeCap),
+        stroke_join: funcToPy(() => p5ref.strokeJoin),
+        stroke_weight: funcToPy(() => p5ref.strokeWeight),
+
+        background: funcToPy(() => p5ref.background),
+        stroke: funcToPy(() => p5ref.stroke),
+        fill: funcToPy(() => p5ref.fill),
+        no_stroke: funcToPy(() => p5ref.noStroke),
+        no_fill: funcToPy(() => p5ref.noFill),
+        
+        begin_shape: funcToPy(() => p5ref.beginShape),
+        end_shape: funcToPy(() => p5ref.endShape),
+        vertex: funcToPy(() => p5ref.vertex),
+
+        describe: funcToPy(() => p5ref.describe),
+
+        frame_count: new Sk.builtin.func(getFrameCount),
+
         PI: new Sk.builtin.float_(Math.PI),
         QUARTER_PI: new Sk.builtin.float_(Math.PI / 4),
         TWO_PI: new Sk.builtin.float_(Math.PI * 2),
+
         WEBGL: new Sk.builtin.str("webgl"),
-        frame_count: new Sk.builtin.func(getFrameCount),
-        fill: new Sk.builtin.func(fill),
-        no_stroke: new Sk.builtin.func(noStroke),
+
+        LABEL: new Sk.builtin.str("label"),
+        FALLBACK: new Sk.builtin.str("fallback"),
+
+        OPEN: new Sk.builtin.str("open"),
+        PIE: new Sk.builtin.str("pie"),
+        CHORD: new Sk.builtin.str("chord"),
+
+        CENTER: new Sk.builtin.str("center"),
+        RADIUS: new Sk.builtin.str("radius"),
+        CORNER: new Sk.builtin.str("corner"),
+        CORNERS: new Sk.builtin.str("corners"),
+
+        ROUND: new Sk.builtin.str("round"),
+        SQUARE: new Sk.builtin.str("butt"),
+        PROJECT: new Sk.builtin.str("square"),
+
+        MITER: new Sk.builtin.str("miter"),
+        BEVEL: new Sk.builtin.str("bevel"),
+
+        POINTS: new Sk.builtin.int_(0),
+        LINES: new Sk.builtin.int_(1),
+        TRIANGLES: new Sk.builtin.int_(4),
+        TRIANGLE_FAN: new Sk.builtin.int_(6),
+        TRIANGLE_STRIP: new Sk.builtin.int_(5),
+        QUADS: new Sk.builtin.str("quads"),
+        QUAD_STRIP: new Sk.builtin.str("quad_strip"),
+        TESS: new Sk.builtin.str("tess"),
+
+        CLOSE: new Sk.builtin.str("close"),
     };
+
+    Object.assign(module, moduleContent);
 
     return module;
 };
